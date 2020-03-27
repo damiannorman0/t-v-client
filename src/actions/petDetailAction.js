54641ce07d7dcd5175d66240fdcb534379bec773
Damian Norman <damiannorman@gmail.com>
2020-03-27 00:44:39 -0400

    api data for al pets

--- src/actions/petDetailAction.js
+++ src/actions/petDetailAction.js
@@ -15,7 +15,6 @@ export const petDetailAction = (id) => {
 					}
 				});
 			}).catch(error => {
-				debugger
 				reject(error);
 			});
 		});
--- src/actions/petsAction.js
+++ src/actions/petsAction.js
@@ -1,178 +1,22 @@
+const axios = require('axios');
+
 export const petsAction = () => {
 	return dispatch => {
 		dispatch({
 			type: 'LOADING_ACTION'
 		});
-		let t = setTimeout(e => {
-			clearTimeout(t);
-			dispatch({
-				type: 'GET_PETS_ACTION',
-				payload: [{
-						"id" : 11,
-						"created_at": "2015-04-21T10:44:51-06:00",
-						"end": "2015-04-24T07:30:00-06:00",
-						"start": "2015-04-24T07:00:00-06:00",
-						"updated_at": "2015-04-21T10:44:51-06:00",
-						"name": "Gregory House",
-						"type": "cat",
-						"conditions": [],
-						"image": ""
-				}, {
-					"id" : 12,
-					"created_at": "2015-04-21T10:44:51-06:00",
-					"end": "2015-04-24T07:30:00-06:00",
-					"start": "2015-04-24T07:00:00-06:00",
-					"updated_at": "2015-04-21T10:44:51-06:00",
-					"name": "Gregory House",
-					"type": "cat",
-					"conditions": [],
-					"image": ""
-				}, {
-					"id" : 13,
-					"created_at": "2015-04-21T10:44:51-06:00",
-					"end": "2015-04-24T07:30:00-06:00",
-					"start": "2015-04-24T07:00:00-06:00",
-					"updated_at": "2015-04-21T10:44:51-06:00",
-					"name": "Gregory House",
-					"type": "cat",
-					"conditions": [],
-					"image": ""
-				},
-					{
-						"id" : 14,
-						"created_at": "2015-04-21T10:44:51-06:00",
-						"end": "2015-04-24T07:30:00-06:00",
-						"start": "2015-04-24T07:00:00-06:00",
-						"updated_at": "2015-04-21T10:44:51-06:00",
-						"name": "Gregory House",
-						"type": "cat",
-						"conditions": [],
-						"image": ""
-					}, {
-						"id" : 11,
-						"created_at": "2015-04-21T10:44:51-06:00",
-						"end": "2015-04-24T07:30:00-06:00",
-						"start": "2015-04-24T07:00:00-06:00",
-						"updated_at": "2015-04-21T10:44:51-06:00",
-						"name": "Gregory House",
-						"type": "cat",
-						"conditions": [],
-						"image": ""
-					}, {
-						"id" : 12,
-						"created_at": "2015-04-21T10:44:51-06:00",
-						"end": "2015-04-24T07:30:00-06:00",
-						"start": "2015-04-24T07:00:00-06:00",
-						"updated_at": "2015-04-21T10:44:51-06:00",
-						"name": "Gregory House",
-						"type": "cat",
-						"conditions": [],
-						"image": ""
-					}, {
-						"id" : 13,
-						"created_at": "2015-04-21T10:44:51-06:00",
-						"end": "2015-04-24T07:30:00-06:00",
-						"start": "2015-04-24T07:00:00-06:00",
-						"updated_at": "2015-04-21T10:44:51-06:00",
-						"name": "Gregory House",
-						"type": "cat",
-						"conditions": [],
-						"image": ""
-					},
-					{
-						"id" : 14,
-						"created_at": "2015-04-21T10:44:51-06:00",
-						"end": "2015-04-24T07:30:00-06:00",
-						"start": "2015-04-24T07:00:00-06:00",
-						"updated_at": "2015-04-21T10:44:51-06:00",
-						"name": "Gregory House",
-						"type": "cat",
-						"conditions": [],
-						"image": ""
-					}, {
-						"id" : 11,
-						"created_at": "2015-04-21T10:44:51-06:00",
-						"end": "2015-04-24T07:30:00-06:00",
-						"start": "2015-04-24T07:00:00-06:00",
-						"updated_at": "2015-04-21T10:44:51-06:00",
-						"name": "Gregory House",
-						"type": "cat",
-						"conditions": [],
-						"image": ""
-					}, {
-						"id" : 12,
-						"created_at": "2015-04-21T10:44:51-06:00",
-						"end": "2015-04-24T07:30:00-06:00",
-						"start": "2015-04-24T07:00:00-06:00",
-						"updated_at": "2015-04-21T10:44:51-06:00",
-						"name": "Gregory House",
-						"type": "cat",
-						"conditions": [],
-						"image": ""
-					}, {
-						"id" : 13,
-						"created_at": "2015-04-21T10:44:51-06:00",
-						"end": "2015-04-24T07:30:00-06:00",
-						"start": "2015-04-24T07:00:00-06:00",
-						"updated_at": "2015-04-21T10:44:51-06:00",
-						"name": "Gregory House",
-						"type": "cat",
-						"conditions": [],
-						"image": ""
-					},
-					{
-						"id" : 14,
-						"created_at": "2015-04-21T10:44:51-06:00",
-						"end": "2015-04-24T07:30:00-06:00",
-						"start": "2015-04-24T07:00:00-06:00",
-						"updated_at": "2015-04-21T10:44:51-06:00",
-						"name": "Gregory House",
-						"type": "cat",
-						"conditions": [],
-						"image": ""
-					}, {
-						"id" : 11,
-						"created_at": "2015-04-21T10:44:51-06:00",
-						"end": "2015-04-24T07:30:00-06:00",
-						"start": "2015-04-24T07:00:00-06:00",
-						"updated_at": "2015-04-21T10:44:51-06:00",
-						"name": "Gregory House",
-						"type": "cat",
-						"conditions": [],
-						"image": ""
-					}, {
-						"id" : 12,
-						"created_at": "2015-04-21T10:44:51-06:00",
-						"end": "2015-04-24T07:30:00-06:00",
-						"start": "2015-04-24T07:00:00-06:00",
-						"updated_at": "2015-04-21T10:44:51-06:00",
-						"name": "Gregory House",
-						"type": "cat",
-						"conditions": [],
-						"image": ""
-					}, {
-						"id" : 13,
-						"created_at": "2015-04-21T10:44:51-06:00",
-						"end": "2015-04-24T07:30:00-06:00",
-						"start": "2015-04-24T07:00:00-06:00",
-						"updated_at": "2015-04-21T10:44:51-06:00",
-						"name": "Gregory House",
-						"type": "cat",
-						"conditions": [],
-						"image": ""
-					},
-					{
-						"id" : 14,
-						"created_at": "2015-04-21T10:44:51-06:00",
-						"end": "2015-04-24T07:30:00-06:00",
-						"start": "2015-04-24T07:00:00-06:00",
-						"updated_at": "2015-04-21T10:44:51-06:00",
-						"name": "Gregory House",
-						"type": "cat",
-						"conditions": [],
-						"image": ""
-					}]
+
+		return new Promise((resolve, reject) => {
+			axios.get(`http://localhost:7000/api/pets/`).then(({data}) => {
+				dispatch({
+					type: 'GET_PETS_ACTION',
+					payload: [
+						...data
+					]
+				});
+			}).catch(error => {
+				reject(error);
 			});
-		}, 1000);
+		});
 	};
 };
