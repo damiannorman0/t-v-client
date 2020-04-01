c0fa03f76f48b8f7a144e31fe6d7df7739a911de
Damian Norman <damiannorman@gmail.com>
2020-04-01 16:55:20 -0400

    added default action param

--- src/reducers/petsReducer.js
+++ src/reducers/petsReducer.js
@@ -6,7 +6,7 @@ const initialState = {
 	}
 };
 
-export default (state = initialState, action) => {
+export default (state = initialState, action = {}) => {
 	switch (action.type) {
 		case 'LOADING_ACTION':
 			return {
